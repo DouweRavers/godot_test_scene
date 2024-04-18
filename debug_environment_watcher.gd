96d2d2c19892ec4c7b4d5074c11bdca5a57f1b2f
Douwe Ravers <douwe.ravers@kuleuven.be>
2024-04-18 11:43:15 +0200

    Correction replacement bug.

--- debug_environment_watcher.gd
+++ debug_environment_watcher.gd
@@ -5,10 +5,10 @@ func _ready():
 	var path = DebugEnvironmentLib.get_debug_path(node)
 	if not FileAccess.file_exists(path): return
 	var debug_env = load(path).instantiate()
+	replace_by.call_deferred(debug_env)
 	var target = debug_env.find_child("$DEBUG_TARGET$")
 	if target == null: 
 		node.add_child(debug_env)
 	else:
-		node.replace_by.call_deferred(debug_env)
+		node.get_parent().remove_child.call_deferred(node)
 		target.replace_by.call_deferred(node)
-		target.free()
