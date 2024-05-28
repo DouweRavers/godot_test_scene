5a3ab9bfb726fd7becd335b74c809fcce35415b5
Douwe Ravers <douwe.ravers@kuleuven.be>
2024-05-28 13:35:19 +0200

    Updated paths

--- plugin.gd
+++ plugin.gd
@@ -4,8 +4,8 @@ extends EditorPlugin
 var plugin
 
 func _enter_tree():
-	plugin = preload("res://addons/douwco.debug_environments/debug_environment_editor_inspector.gd").new()
-	add_autoload_singleton("DebugEnvironmentWatcher", "res://addons/douwco.debug_environments/debug_environment_watcher.gd")
+	plugin = preload("res://addons/godot_debug_environments/debug_environment_editor_inspector.gd").new()
+	add_autoload_singleton("DebugEnvironmentWatcher", "res://addons/godot_debug_environments/debug_environment_watcher.gd")
 	add_inspector_plugin(plugin)
 
 func _exit_tree():
