2d78d41306b3367c836264335480820c75e852fa
DouweRavers <douwe.ravers@hotmail.com>
2026-02-27 16:19:33 +0100

    Fixed the not find sibling button bug.

--- addons/godot_test_scene/plugin.gd
+++ addons/godot_test_scene/plugin.gd
@@ -11,13 +11,14 @@ func _exit_tree() -> void:
 
 func add_play_test_scene_button()->void:
 	var play_current_button := search_play_current_button(EditorInterface.get_base_control())
-	var play_test_button_scene := load("res://addons/godot_test_scene/test_scene_button.tscn") as PackedScene
+	var play_test_button_scene := load("uid://dbjvqwv7nf7e") as PackedScene
 	play_test_button = play_test_button_scene.instantiate()
 	play_current_button.add_sibling(play_test_button)
 
 func search_play_current_button(node:Node)->Button:
-	if node is Button and (node as Button).tooltip_text == "Play the edited scene.":
-		return (node as Button)
+	if node is Button and (node as Button).name == "@Button@4775":
+		# Alternative: button.tooltip_text == "Play the currently edited scene."
+		return node as Button
 	for child in node.get_children():
 		var button := search_play_current_button(child)
 		if button is Button:
